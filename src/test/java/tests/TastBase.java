52b95ab971bd990431fa433c22c9f81b7fd8cb9a
Taygib <nfsmwm5@mail.ru>
2023-04-08 15:28:48 +0300

    mobile and browserstack

---
+++ src/test/java/tests/TastBase.java
@@ -0,0 +1,34 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import drivers.BrowserstackMobileDriver;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TastBase {
+
+    @BeforeAll
+    static void beforeAll () {
+        Configuration.browser = BrowserstackMobileDriver.class.getName();
+        Configuration.browserSize = null;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open();
+    }
+
+    @AfterEach
+    void afterEach () {
+
+
+        closeWebDriver();
+    }
+}
